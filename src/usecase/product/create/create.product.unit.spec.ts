457015f95b5c66d008d4cee16474e50a9ca99e58
Thayane Santana <thayane.santana@ifood.com.br>
2023-05-08 10:55:13 +0200

    feat: add create product use case

---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,59 @@
+import CreateProductUseCase from "./create.product.usecase";
+
+const input = {
+    type: "a",
+    name: "product 1",
+    price: 10
+}
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        update: jest.fn(),
+        find: jest.fn(),
+        findAll: jest.fn()
+    };
+};
+
+describe("Unit test create product unit test", () => {
+    it("should create a product", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        const output = await productCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        });
+    });
+
+    it("it should throw an error when name is missing", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        const new_input = {
+            type: input.type,
+            name: "",
+            price: input.price
+        }
+        await expect(productCreateUseCase.execute(new_input)).rejects.toThrow(
+            "Name is required"
+          );
+    });
+
+    it("it should throw an error when pricr is negative", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        const new_input = {
+            type: input.type,
+            name: input.name,
+            price: -1
+        }
+        await expect(productCreateUseCase.execute(new_input)).rejects.toThrow(
+            "Price must be greater than zero"
+          );
+    });
+});
